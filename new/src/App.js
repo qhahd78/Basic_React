b2395dc983c7ea06fc4a294b75311769715d3663
YuMinHa <qhahd78>
2020-12-23 20:14:57 +0900

    Event

--- new/src/App.js
+++ new/src/App.js
@@ -20,10 +20,13 @@ import Subject from './components/Subject';
 //부모(App) state 라고 하는 내부 정보. 
 class App extends Component {
 //state 선언 전에 넣어주어야 함. (constructor, super)
+//constructor 에서는 편하게 state 값들을 바꾸어도 된다. 
   constructor(props){
     super(props);
     this.state = {
-      mode:'welcome',
+      mode:'read',
+      //기본으로 2번이 선택. 
+      selected_content_id:2,
       subject:{title:'WEB', sub: 'World Wide Web!'},
       welcome:{title:'Welcome', desc:'Hello, React'},
       //여러개라서 배열 형태로 state 생성
@@ -44,19 +47,63 @@ class App extends Component {
       _desc = this.state.welcome.desc;
     } else if(this.state.mode === 'read'){
       //read 일 경우 아래 코드 실행. 
-      _title = this.state.contents[0].title;
-      _desc = this.state.contents[0].desc;
-
+      var i = 0;
+      while(i < this.state.contents.length){
+        //해당 번호와 같으면, 그 해당 번호의 내용을 출력. 
+        var data = this.state.contents[i];
+        if(data.id === this.state.selected_content_id){
+          _title = data.title;
+          _desc = data.desc;
+          break;
+        }
+        i = i + 1;
+      }
     }
     return (
       <div className="App">
         <Subject 
-        title={this.state.subject.title}
-        sub={this.state.subject.sub}>
+          title={this.state.subject.title}
+          sub={this.state.subject.sub}
+          // Subject (Web) 을 클릭하면, mode가 welcome 으로 변경되고, 이에 따라 html 내용도 바뀐다. 
+          onChangePage={function(){
+            this.setState({mode:'welcome'});
+          }.bind(this)}
+        >
         </Subject>
-        <Subject title="React" sub="For UI"></Subject>
-{/* // 아래처럼 자식한테 전달할 때 props 형태로 전달.  props 이름: data*/}
-        <TOC data={this.state.contents}></TOC>
+        {/* <header> 
+            <h1><a href="/" onClick={function(e){
+              console.log(e);
+              e.preventDefault();
+              e 태그의 기본적인 동작을 막는다. 페이지의 리로드를 막음. 
+              debugger=실행을 멈춤.
+              this.state.mode = 'welcome';
+              클릭시 state mode 를 welcome 으로 변경. --에러 발생, state 변경이 안 된다. 
+              this.setState({
+                 mode: 'welcome'
+              });
+              setState로 state 값을 바꿔준다. 
+              함수의 형태로 state 값 변경.
+              setState 함수 안에 바꾸고 싶은 것을 객체 형태로 넣어서 바꿈. 
+              state 몰래 바꾸는 거라고 생각하면 된다 .
+            }.bind(this)}>{this.state.subject.title}</a></h1>
+            이 앱의 객체를 함수랑 엮어준다.
+            bind=엮는다. 
+            bind 추가-- this에 아무 값도 없었는데 이 컴포넌트를 this 로 지정해준다.
+            {this.state.subject.sub}
+        </header>
+아래처럼 자식한테 전달할 때 props 형태로 전달.  props 이름: data */}
+        <TOC
+        //이벤트 생성 
+          onChangePage={function(id){
+            this.setState({
+              mode:'read',
+              //Number 함수로 id 값(문자로 넘어왔던 것)을 숫자로 변경 
+              selected_content_id:Number(id)
+            });
+        //state 변경 
+          }.bind(this)}
+          data={this.state.contents}
+        ></TOC>
         <Content title={_title} desc={_desc}></Content>
       </div>
     );
--- new/src/components/Subject.js
+++ new/src/components/Subject.js
@@ -4,7 +4,11 @@ class Subject extends Component{
     render () {
       return (
         <header>
-            <h1><a href="">{this.props.title}</a></h1>
+            <h1><a href="" onClick={function(e){
+              e.preventDefault();
+              // onChangePage 함수를 호출한다. 
+              this.props.onChangePage();
+            }.bind(this)}>{this.props.title}</a></h1>
             {this.props.sub}
         </header>
       );
